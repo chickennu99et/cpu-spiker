15a952cf9249cdb7a092bd1e850ac1e35612f359
chicken nugget <116309083+chickennu99et@users.noreply.github.com>
2023-02-08 16:25:58 -0600

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace oeeooe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = 1;
+            while (v >= 1)
+            {
+                v++;
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
